8b380358c8baa66b2c67329c9f230b9efbedfec0
Ebendttl <ebendttl@gmail.com>
2024-11-11 22:42:33 +0100

    Updated the file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,34 +1,27 @@
 #include "hash_tables.h"
-/* Task 0. >>> ht = {}: a function that creates a hash table. */
-/**
- * hash_table_create - creates a hash table
- * @size: size of the array
- *
- * Return: pointer to the newly created hash table
- */
 
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *ht;
 	unsigned long int i;
 
-    /* Allocate memory for the hash table. */
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
 		return (NULL);
 
-	hash_table->size = size;
-    /* Allocate memory for the array of pointers to the nodes. */
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
 		return (NULL);
-	}
-
 	for (i = 0; i < size; i++)
-    /* Initialize the array of pointers to the nodes. */
-		hash_table->array[i] = NULL;
-    /* Return the pointer to the hash table. */
-	return (hash_table);
+		ht->array[i] = NULL;
+
+	return (ht);
 }
